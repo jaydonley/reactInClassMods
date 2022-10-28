0d7a09012e09f8fd6fc1323c360a6724fc7bd8f6
Jason Donley <jay.donley@gmail.com>
2022-10-27 23:25:19 -0400

    reactInClassMods

---
+++ app.js
@@ -0,0 +1,54 @@
+// ------------react lesson 1 -------------
+
+// everything installed
+
+
+// ----------- React Lesson 2 ------------
+
+    render (){
+        return(
+            
+        <div className="properClassName">
+            <h1>Heeeeey Deeeevs!</h1>
+            <p>{ Math.random() * 10 }</p>
+        </div>
+        )
+    }
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
+
+
+
+
+// ----------- Mod: React Lesson 3 ------------
+
+// class App extends React.Component {
+//     state ={
+//         name: "Marcus",
+//         age: 103
+//     }
+
+
+
+//     render (){
+//         return(
+            
+//         <div className="properClassName">
+//             <h1>Heeeeey Deeeevs!</h1>
+//             <p>My name is: { this.state.name } and I am {this.state.age}</p>
+//         </div>
+//         )
+//     }
+
+// }
+
+// ReactDOM.render(<App />, document.getElementById('app'))
+
+// ------------ React Lesson 4 ------------------
+
+//    n/a
+
+// -------------------- react lesson 5 -----------
